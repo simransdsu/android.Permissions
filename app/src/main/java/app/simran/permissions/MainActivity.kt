4f8a0dc53695deb0d725cfbb38ad6497bc136e4b
Simran <simran5590@gmail.com>
2020-12-15 17:19:11 -0800

    requesting permissions

--- app/src/main/java/app/simran/permissions/MainActivity.kt
+++ app/src/main/java/app/simran/permissions/MainActivity.kt
@@ -1,11 +1,71 @@
 package app.simran.permissions
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnRequestPermission: Button
+
+    val TAG = "MainActivityTAG"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnRequestPermission = findViewById(R.id.btnRequestPermissions)
+        btnRequestPermission.setOnClickListener { requestPermissions() }
+    }
+
+    // CHECKING WHETHER THE FOLLOWING PERMISSIONS ARE GRANTED OR NOT
+    private fun hasWriteExternalStoragePermission()
+            = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+
+    private fun hasLocationForegroundPermission()
+            = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+    private fun hasLocationBackgroundPermission()
+            = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+    // REQUESTING PERMISSIONS TO THE USER TO APPROVE OR DENY IF THEY ALREADY HAVE NOT APPROVED FOR THEM.
+    private fun requestPermissions() {
+        var permissionsToRequest = mutableListOf<String>()
+
+        // CHECKING IF PERMISSIONS ARE ALREADY NOT APPROVED
+        if(!hasWriteExternalStoragePermission()) {
+            permissionsToRequest.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+
+        if(!hasLocationForegroundPermission()) {
+            permissionsToRequest.add(Manifest.permission.ACCESS_COARSE_LOCATION)
+        }
+
+        if(!hasLocationBackgroundPermission()) {
+            permissionsToRequest.add(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+        }
+
+        // NOW REQUESTING PERMISSIONS WHICH ARE NOT APPROVED FROM THE LIST
+        if(permissionsToRequest.isNotEmpty()) {
+            ActivityCompat.requestPermissions(this, permissionsToRequest.toTypedArray(), 0)
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == 0 && grantResults.isNotEmpty()) {
+            for (i in grantResults.indices) {
+                if(grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "${permissions[i]}  granted")
+                }
+            }
+        }
+
     }
 }
